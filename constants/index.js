496bc1c0ebdb8f64cdc079693c487d9cc97960a1
Nils Giebing <nils.giebing@gmail.com>
2022-06-23 12:02:53 +0200

    First commit

---
+++ constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xe67bF3f39c257151821AFE720F777e91a46Df0cf";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
